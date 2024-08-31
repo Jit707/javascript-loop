f29289ebe9070c5d656db436fbdfe270fdc1d67e
Jit <jit.oung254343@gmail.com>
2024-08-31 13:21:13 +0700

    add finished assignment

--- ex-4.js
+++ ex-4.js
@@ -1,6 +1,15 @@
 // Exercise #4: Displaying Restaurant Name
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
-let newRestaurants = [];
+let newRestaurants 
 // Start coding here
 
+for ( let i=0; i<restaurants.length; i++){
+    
+    newRestaurants += `Restaurant name : ${restaurants[i]}`
+    
+    
+}
+
 console.log(newRestaurants);
+;
+
--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,8 @@
 let companyName = "TechUp";
 
 // Start coding here
+
+for (let  i=0 ; i < companyName.length ; i++){
+    console.log(`Number ${[i]} character ${companyName[i]}`);
+    
+}
